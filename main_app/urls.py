89b411c5c91ed6f0d9282d6f9b87a881e475b626
smichaelonline <stephaniemichael001@gmail.com>
2022-11-08 19:21:35 -0500

    set up homepage and rat model

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about')
+  path('about/', views.about, name='about'),
+  path('rats/', views.rats_index, name='cats_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 # Create your views here.
@@ -8,3 +7,19 @@ def home(request):
 
 def about(request): 
   return render(request, 'about.html')
+
+class Rat: 
+  def __init__(self, name, color, description, location): 
+    self.name = name
+    self.color = color
+    self.description = description
+    self.location = location 
+
+rats = [
+  Rat('Splinter', 'black', 'Hangs out in the subway', 'Times Square'),
+  Rat('Pizza Rat', 'grey', 'found some pizza boii', '3rd Ave Subway Station'),
+  Rat('Feral Rat', 'spots', 'screams at pedestrians', 'the street'),
+]
+
+def rats_index(request): 
+  return render(request, 'rats/index.html', {'rats': rats })

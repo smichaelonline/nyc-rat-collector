0b09c5c71ebb9cdaba85db6ea30cde703f01b2a1
smichaelonline <stephaniemichael001@gmail.com>
2022-11-08 19:40:12 -0500

    create migrations for rat model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Rat(models.Model):
+  name = models.CharField(max_length=100)
+  color= models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  location = models.CharField(max_length=150)
+
+  def __str__(self):
+    return self.name
